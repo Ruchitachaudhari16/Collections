fc0b2158442fa94ada95ae565cd8542c2d744c4e
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-07-07 08:27:27 -0400

    [Ruchita] Create integer list

---
+++ CollectionsDemo/CollectionsDemo/List_Demo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsDemo
+{
+    //List<T> class represents the list of objects which can be accessed with index 
+    public class List_Demo
+    {
+        //creating a list of integers.
+        List<int>list= new List<int>();
+
+        public void Adding()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Number of count in a list: " +list.Count);
+        }
+
+    }
+}
---
+++ CollectionsDemo/CollectionsDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsDemo
+{ 
+    public class Program
+    {
+        //Collection is a class that means we can create an object.
+        //can be create or declared instance of a class, so we can add a data in collections
+        static void Main(string[] args)
+        {
+            List_Demo list = new List_Demo();
+            list.Adding();
+            Console.ReadLine();
+        }
+    }
+}
